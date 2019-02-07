88f6654a1b4f9caf357d2b814a1ace1dd345a5a9
Dmytro Kutko <dmytrokutko@gmail.com>
2019-02-07 12:17:35 +0200

    snapshot listener (les4)

--- app/src/main/java/com/example/user/lessonsfb/MainActivity.java
+++ app/src/main/java/com/example/user/lessonsfb/MainActivity.java
@@ -14,10 +14,15 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.ListenerRegistration;
 
 import java.util.HashMap;
 import java.util.Map;
+
+import javax.annotation.Nullable;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -40,6 +45,27 @@ public class MainActivity extends AppCompatActivity {
 
         initView();
         initListener();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        noteRef.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
+                if (e != null) {
+                    Toast.makeText(MainActivity.this, "Error loading", Toast.LENGTH_SHORT).show();
+                    Log.d(TAG, "onEvent: " + e.toString());
+                    return;
+                }
+                if (documentSnapshot.exists()) {
+                    String title = documentSnapshot.getString(KEY_TITLE);
+                    String description = documentSnapshot.getString(KEY_DESCRIPTION);
+
+                    tvData.setText("Title: " + title + "\n" + "Description: " + description);
+                }
+            }
+        });
     }
 
     private void initListener() {
