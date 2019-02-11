1f9b9d0c13b1192f4e494ed249aaf6a6df03a339
Dmytro Kutko <dmytrokutko@gmail.com>
2019-02-11 10:32:32 +0200

    list in object (les 16)

--- app/src/main/java/com/example/user/lessonsfb/MainActivity.java
+++ app/src/main/java/com/example/user/lessonsfb/MainActivity.java
@@ -1,40 +1,33 @@
 package com.example.user.lessonsfb;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.user.lessonsfb.model.Note;
-import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
-import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
-import com.google.firebase.firestore.Transaction;
-import com.google.firebase.firestore.WriteBatch;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
 
-    private EditText etTitle, etDescription, etPriority;
+    private EditText etTitle, etDescription, etPriority, etTags;
     private Button btnAdd, btnLoad;
     private TextView tvData;
 
     private FirebaseFirestore store;
     private CollectionReference notebookRef;
-    private DocumentSnapshot lastResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,25 +36,13 @@ public class MainActivity extends AppCompatActivity {
 
         initView();
         initListener();
-        executeTransaction();
+        updateArray();
     }
 
-    private void executeTransaction() {
-        store.runTransaction(new Transaction.Function<Long>() {
-            @Override
-            public Long apply(@NonNull Transaction transaction) throws FirebaseFirestoreException {
-                DocumentReference noteRef = notebookRef.document("New Note");
-                DocumentSnapshot documentSnapshot = transaction.get(noteRef);
-                long newPriority = documentSnapshot.getLong("priority") + 1;
-                transaction.update(noteRef, "priority", newPriority);
-                return newPriority;
-            }
-        }).addOnSuccessListener(new OnSuccessListener<Long>() {
-            @Override
-            public void onSuccess(Long aLong) {
-                Toast.makeText(MainActivity.this, "New Priority: " + aLong, Toast.LENGTH_SHORT).show();
-            }
-        });
+    private void updateArray() {
+        notebookRef.document("x6R9ypYKN5c1ktBi875d")
+//                .update("tags", FieldValue.arrayUnion("new tag"))
+                .update("tags", FieldValue.arrayRemove("new tag"));
     }
 
     public void initListener() {
@@ -86,6 +67,7 @@ public class MainActivity extends AppCompatActivity {
         etTitle = findViewById(R.id.etTitle);
         etDescription = findViewById(R.id.etDescription);
         etPriority = findViewById(R.id.etPriority);
+        etTags = findViewById(R.id.etTags);
         btnAdd = findViewById(R.id.btnAdd);
         btnLoad = findViewById(R.id.btnLoad);
         tvData = findViewById(R.id.tvData);
@@ -101,41 +83,29 @@ public class MainActivity extends AppCompatActivity {
 
         int priority = Integer.parseInt(etPriority.getText().toString());
 
-        Note note = new Note(title, description, priority);
+        String tagInput = etTags.getText().toString();
+        String[] tagArray = tagInput.split("\\s*,\\s*");
+        List<String> tags = Arrays.asList(tagArray);
+
+        Note note = new Note(title, description, priority, tags);
 
         notebookRef.add(note);
     }
 
     public void loadNotes(View v) {
-        Query query;
-
-        if (lastResult == null) {
-            query = notebookRef.orderBy("priority")
-                    .limit(3);
-        } else {
-            query = notebookRef.orderBy("priority")
-                    .startAfter(lastResult)
-                    .limit(3);
-        }
-
-        query.get()
+        notebookRef.whereArrayContains("tags", "tag3").get()
                 .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
                     @Override
                     public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
                         StringBuilder data = new StringBuilder();
-                        for (QueryDocumentSnapshot queryDocumentSnapshot : queryDocumentSnapshots) {
-                            Note note = queryDocumentSnapshot.toObject(Note.class);
-                            note.setDocumentId(queryDocumentSnapshot.getId());
-                            data.append(note.toString());
+                        for (QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots) {
+                            Note note = documentSnapshot.toObject(Note.class);
+                            note.setDocumentId(documentSnapshot.getId());
+
+                            data.append(note.toString()).append("\n");
                         }
-                        if (queryDocumentSnapshots.size() > 0) {
-                            data.append("\n_________________________________\n");
-                            tvData.append(data);
-                            lastResult = queryDocumentSnapshots.getDocuments()
-                                    .get(queryDocumentSnapshots.size() - 1);
-                        }
+                        tvData.setText(data);
                     }
                 });
-
     }
 }
--- app/src/main/java/com/example/user/lessonsfb/model/Note.java
+++ app/src/main/java/com/example/user/lessonsfb/model/Note.java
@@ -2,6 +2,8 @@ package com.example.user.lessonsfb.model;
 
 import com.google.firebase.firestore.Exclude;
 
+import java.util.List;
+
 public class Note {
 
     private String documentId;
@@ -9,15 +11,17 @@ public class Note {
     private String description;
     private long unixTime;
     private int priority;
+    List<String> tags;
 
     public Note() {
     }
 
-    public Note(String title, String description, int priority) {
+    public Note(String title, String description, int priority, List<String> tags) {
         this.title = title;
         this.description = description;
         this.unixTime = System.currentTimeMillis();
         this.priority = priority;
+        this.tags = tags;
     }
 
     @Exclude
@@ -61,12 +65,21 @@ public class Note {
         this.priority = priority;
     }
 
+    public List<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<String> tags) {
+        this.tags = tags;
+    }
+
     @Override
     public String toString() {
         return "Document ID: " + documentId + '\n' +
                 "Title: " + title + '\n' +
                 "Description: " + description + '\n' +
                 "UnixTime: " + unixTime + "\n" +
-                "Priority: " + priority + "\n";
+                "Priority: " + priority + "\n" +
+                "Tags: " + tags.toString() + "\n";
     }
 }
